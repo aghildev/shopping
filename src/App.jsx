3f337d5cc622865f61bd9d72859ce841f3b3c1ba
aghildev <aghilpwilson1994@gmail.com>
2023-12-05 18:10:53 +0530

    Route layout done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./Layout/RootLayout";
+import Error from "./components/Error";
+import Products from "./components/Products";
 const App = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <RootLayout />,
+      errorElement: <Error />,
+      children: [
+        {
+          path: "/",
+          element: <Products />,
+        },
+      ],
     },
   ]);
 

68315b296ff0dadee82c5f84824359deff667a52
aghildev <aghilpwilson1994@gmail.com>
2023-12-05 17:02:17 +0530

    App component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,9 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const App = () => {
+  const router = createBrowserRouter([{}]);
 
+  return <RouterProvider router={router} />;
+};
+
+export default App;

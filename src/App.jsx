137612e031025602fdad81aa378d0d9af85a3375
aghildev <aghilpwilson1994@gmail.com>
2023-12-05 16:59:42 +0530

    cleared

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,1 @@
-import HomePage from "./pages/HomePage";
 
-function App() {
-  return (
-    <>
-      <HomePage />
-    </>
-  );
-}
-
-export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
+      <Outlet />
+      <Footer />
     </div>
   );
 };

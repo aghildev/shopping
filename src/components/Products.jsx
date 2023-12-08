ea2407c9b74f78c23e5ebfdce6aa01def181b736
aghildev <aghilpwilson1994@gmail.com>
2023-12-08 18:45:06 +0530

    products fetched

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,30 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import styles from "../styles/productcard.module.css";
 const Products = () => {
-  return <div>Products</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const res = await fetch("https://fakestoreapi.com/products");
+      const products = await res.json();
+      setData(products);
+    };
+    getProducts();
+  }, []);
+  console.log(data);
+  return (
+    <div className={styles.productContainer}>
+      {data.map((item) => {
+        return (
+          <div key={item.id} className={styles.productCard}>
+            <img src={item.image} alt="" />
+            <h2>{item.title}</h2>
+            <h3>${item.price}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Products;

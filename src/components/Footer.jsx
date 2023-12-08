03a3e2ecb94a4d3e9ec3ec06130d5df7c16ae63d
aghildev <aghilpwilson1994@gmail.com>
2023-12-08 18:21:52 +0530

    Footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import styles from "../styles/footer.module.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.footer}>
+      <p>All Rights Reserved..Aghil Shoppy</p>
+    </div>
+  );
 };
 
 export default Footer;

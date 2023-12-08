20be459a230cf94e28b7b5a2c649f64fd175987b
aghildev <aghilpwilson1994@gmail.com>
2023-12-08 18:13:47 +0530

    Navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,10 @@ import styles from "../styles/navbar.module.css";
 const Navbar = () => {
   return (
     <nav className={styles.navContainer}>
-      <div>Aghil Shoppy !</div>
-      <ul>
+      <div>
+        <h1>Aghil Shoppy !</h1>
+      </div>
+      <ul className={styles.navItems}>
         <li>Home</li>
         <li>About</li>
         <li>Cart</li>

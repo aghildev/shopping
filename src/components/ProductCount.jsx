9be6942ce8e67379280c54d415a2ae8efcf86408
aghildev <aghilpwilson1994@gmail.com>
2024-01-09 09:58:00 +0530

    cart page added

---
+++ src/components/ProductCount.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProductCount = () => {
+  return (
+    <div>
+      <button>-</button>
+      <p>1</p>
+      <button>+</button>
+    </div>
+  );
+};
+
+export default ProductCount;
